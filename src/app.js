da9d8497216662149ef699617dac81c85921304e
AcheZeta <ache.mariana@gmail.com>
2019-12-10 19:45:18 -0600

    Feat: Directives

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ new Vue({
         return {
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 0
+            changePercent: 0,
+            pricesWithDays: [{
+                    day: 'Lunes',
+                    value: 8613
+                },
+                {
+                    day: 'Martes',
+                    value: 41373
+                },
+                {
+                    day: 'Miércoles',
+                    value: 45768
+                },
+                {
+                    day: 'Jueves',
+                    value: 56780
+                },
+                {
+                    day: 'Viernes',
+                    value: 19942
+                },
+                {
+                    day: 'Sábado',
+                    value: 45670
+                },
+                {
+                    day: 'Domingo',
+                    value: 78930
+                }
+            ],
+            showPrices: false
+        }
+    },
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
         }
     }
 })
